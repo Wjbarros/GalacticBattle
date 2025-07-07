5f97701030d6bc71ef12a26d93186b08ff9ee966
Wenderson <wendersonbarros@gmail.com>
2025-07-07 20:43:53 -0300

    Creating class

--- code/Player.py
+++ code/Player.py
@@ -24,15 +24,14 @@ class Player(Entity):
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
             self.rect.centerx += ENTITY_SPEED[self.name]
         pass
-
     def shoot(self):
-        self.shot_delay -= 1
-        if self.shot_delay == 0:
-            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
-            pressed_key = pygame.key.get_pressed()
-            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
-                return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+            self.shot_delay -= 1
+            if self.shot_delay == 0:
+                self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+                pressed_key = pygame.key.get_pressed()
+                if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                    return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+                else:
+                    return None
             else:
                 return None
-        else:
-            return None
